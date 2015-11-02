19abf812c2e48532fe8e415e854a5d3abf17462a
ryan heupler & alyssa horrocks <student@epicodus.com>
2015-11-02 10:44:47 -0800

    adds index page displaying messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,9 @@
 class MessagesController < ApplicationController
 
+  def index
+    @messages = Message.all
+  end
+
   def new
     @message = Message.new
   end
@@ -7,8 +11,15 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new()
     if @message.save
-      flash[:notice]
+      flash[:notice] = "Your message was sent!"
+      redirect_to messages_path
+    else
+      render 'new'
     end
+  end
+
+  def show
+    @message = Message.find(params[:id])
   end
 
   private
